c52f454f38a46c2e77b89392bad303211655b112
github-main-user <kolya.skripn@mail.ru>
2025-10-01 17:26:21 +0300

    chore: register models in admin panel

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
-# Register your models here.
+from .models import NetworkNode, Product
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "model", "node", "release_date")
+
+
+@admin.action(description="Очистить задолженность у выбранных")
+def clear_debt(modeladmin, request, queryset):
+    queryset.update(debt=0)
+
+
+@admin.register(NetworkNode)
+class NetworkNodeAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "level",
+        "country",
+        "city",
+        "debt",
+        "created_at",
+    )
+    list_filter = ("city", "country", "level")
+    search_fields = ("name", "city", "country", "email")
+    actions = [clear_debt]
+    readonly_fields = ("level", "created_at", "supplier_link")
+
+    def supplier_link(self, obj):
+        if obj.supplier:
+            return format_html(
+                '<a href="/admin/network/networknode/{}/change/">{}</a>',
+                obj.supplier.pk,
+                obj.supplier.name,
+            )
+        return "-"
+
+    supplier_link.short_description = "Ссылка на поставщика"
