d22f1b7e283f053459aaf747af85a0c5430ed309
github-main-user <kolya.skripn@mail.ru>
2025-10-03 20:23:46 +0300

    feat: make session and token authentications default

--- config/settings.py
+++ config/settings.py
@@ -90,6 +90,10 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ],
 }
 
 SPECTACULAR_SETTINGS = {
